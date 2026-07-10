e64db106567c52a64b2795965b04b53a448b5e2f
yalidiqbal01 <yalidiqbal01@gmail.com>
2026-07-10 19:26:16 +0600

    Initial project setup

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>EDU Track</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <button class="btn btn-primary">
+    Bootstrap Button
+</button>
+
+<h1>Welcome to EDU Track</h1>
+
+<?php
+echo "Hello Yalid!";
+?>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - EDU Track</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<div class="container mt-5">
+
+    <h2 class="text-center mb-4">
+        Student Registration
+    </h2>
+
+    <div class="card p-4">
+
+        <form>
+
+            <div class="mb-3">
+                <label>Full Name</label>
+                <input type="text" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="email" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label>Password</label>
+                <input type="password" class="form-control">
+            </div>
+
+            <button class="btn btn-success w-100">
+                Register
+            </button>
+
+        </form>
+
+    </div>
+
+</div>
+
+</body>
+</html>
